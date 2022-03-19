4956f1b342c0f02211a0b54ae92ce30caa384c63
HARI_PRASAD <96721366+HARIPRASAD27102003@users.noreply.github.com>
2022-03-19 18:07:20 +0530

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import avatarImage from "../assets/avatarImage.jpeg";
 export default function Testimonials() {
   return (
     <Section id="testimonials">
